2dbaccd24b928b6ee0ba074104bbcad63afa481c
xinleiye <xinlei_ye@163.com>
2017-10-20 15:07:05 +0800

    任务功能完善2
    1. 增加已有任务编辑功能
    2. 正确显示未完成任务数量

--- todo/app.js
+++ todo/app.js
@@ -17,20 +17,51 @@ var app = new Vue({
     el: ".main",
     data: {
         todoList: todoList,
-        todoNew: ""
+        // 保存新增的计划
+        todoNew: "",
+        // 保存正在编辑的计划
+        editingTodo: "",
+        // 保存计划的原值，用于按esc取消编辑时，还原原值
+        oldTodo: ""
+    },
+    computed:{
+        unCompleteTodo() {
+            return this.todoList.filter((item) => {return !item.isChecked;}).length;
+        }
     },
     methods: {
-        addTodoNew(){
+        addTodoNew() {
             this.todoList.push({
                 todoTitle: this.todoNew,
                 isChecked: false
             });
             this.todoNew = "";
         },
-        deleteTodo(item){
+        deleteTodo(item) {
             var pos = this.todoList.indexOf(item);
             if (pos !== -1) {
                 this.todoList.splice(pos, 1);
+            }
+        },
+        editTodo(item) {
+            this.oldTodo = item.todoTitle;
+            this.editingTodo = item;
+        },
+        editTodoSuccess() {
+            this.editingTodo = "";
+        },
+        editTodoCancel(item) {
+            item.todoTitle = this.oldTodo;
+            this.oldTodo = "";
+            this.editingTodo = "";
+        }
+    },
+    directives: {
+        "focus": {
+            update(elem, binding) {
+                if (binding.value) {
+                    elem.focus();
+                }
             }
         }
     }
